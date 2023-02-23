0cac87aa88e233108efb7a7e3d82a395d0654f53
Jeon_minGyu <coby4313@gmail.com>
2023-02-23 19:22:45 +0900

    API수정

--- GuestBook/views.py
+++ GuestBook/views.py
@@ -2,11 +2,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializer import CommentListGetSerializer, CommentCreateSerializer
+from .serializer import CommentSerializer, CommentCreateSerializer
 from .models import Comment
 
-@api_view(['GET'])
-def get_comment_list(request):
+@api_view(['GET', 'POST'])
+def comment(request):
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print(request.method)
+    print()
+    print()
+    print()
+    print()
+    print()
     if request.method == "GET":
         comment_list = Comment.objects.all()
         
@@ -14,13 +26,12 @@ def get_comment_list(request):
             return Response(status=404)
         
         else:
-            serializer = CommentListGetSerializer(comment_list, many = True)
+            serializer = CommentSerializer(comment_list, many = True)
             return Response(serializer.data, status=200)
-
-@api_view(['POST'])
-def create_comment(request):
-    serializer = CommentCreateSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "POST":
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
