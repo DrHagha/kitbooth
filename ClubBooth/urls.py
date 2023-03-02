b71618e18cd9ecfc764df665969856bb39587fed
Jeon_minGyu <coby4313@gmail.com>
2023-03-02 21:28:16 +0900

    설정 수정

--- ClubBooth/urls.py
+++ ClubBooth/urls.py
@@ -15,6 +15,4 @@ urlpatterns = [
     path('create/',boothcreate)
 ]
 
-##전체 정보 조회(http://127.0.0.1:8000/booth/all/)(req: {} -> res : {{id : 1, booth_name : ddd, booth_location : s23}, {...}} )
-#특정 데이터 조회(http://127.0.0.1:8000/booth/detail/<int:pk>)(req : {} -> res : {name : dd, ddd: sss, ...})
 urlpatterns = format_suffix_patterns(urlpatterns)
--- kitbooth/settings.py
+++ kitbooth/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-from mysettings import my_AWS_ACCESS_KEY_ID, my_AWS_SECRET_ACCESS_KEY
+from mysettings import my_AWS_ACCESS_KEY_ID, my_AWS_SECRET_ACCESS_KEY, my_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xvg)*)%s*i=_u#$syumbmfnt_w-##uiayygqe1t74d$hwy&+^('
+SECRET_KEY = my_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
