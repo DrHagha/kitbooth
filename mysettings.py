a964662902189e56796e3ca554363ac8ec466f7e
Jeon_minGyu <coby4313@gmail.com>
2023-03-04 15:51:26 +0900

    .ignore재설정

--- mysettings.py
+++
@@ -1,3 +0,0 @@
-my_AWS_ACCESS_KEY_ID = 'AKIA6BZET2ZQYANCEIE3'
-my_AWS_SECRET_ACCESS_KEY = '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'
-my_SECRET_KEY = 'django-insecure-xvg)*)%s*i=_u#$syumbmfnt_w-##uiayygqe1t74d$hwy&+^('
