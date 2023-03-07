32e51563b532040b5ed3063ba77543bc74abe090
Jeon_minGyu <coby4313@gmail.com>
2023-03-07 23:13:30 +0900

    get club booth data 'order_by' update

--- ClubBooth/views.py
+++ ClubBooth/views.py
@@ -7,7 +7,7 @@ from .models import Booth
 @api_view(['GET'])
 def booth_list(request):
     if request.method == 'GET':
-        booth_list = Booth.objects.all()
+        booth_list = Booth.objects.all().order_by('booth_location')
         serializer = BoothDetailSerializer(booth_list, many = True)
         return Response(serializer.data, status=200)
     
